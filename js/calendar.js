7f75b0d1063f3f93d37c651e17ec1c75631a9b58
Isaac Bell <isaacbell388@gmail.com>
2016-10-10 04:43:30 -0500

    Calendar

--- js/calendar.js
+++ js/calendar.js
@@ -11,18 +11,18 @@ $.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.m
       center: 'title',
       right: 'month,agendaWeek,agendaDay'
     },
-    timezone: 'local',
     editable: false,
+    timezone: 'local',
     events: [
       {
-        title: "<strong>2016 Seminar</strong> \n AL Fire College, Tuscaloosa, AL. \n To register: contact@al-iasiu.com",
-        start: Date(2016, 11, 10),
-        end: Date(2016, 11, 10)
+        title: "2016 Seminar \n AL Fire College, Tuscaloosa, AL. \n To register: contact@al-iasiu.com",
+        start: new Date(2016, 10, 10),
+        end: new Date(2016, 10, 10)
       },
       {
         title: 'General Business Meeting \n   10:00 am at Allstate Insurance, 2600 Corporate Dr. Ste. 240, Birmingham, AL',
-        start: Date(2016, 12, 08),
-        end: Date(2016, 12, 08)
+        start: new Date(2016, 11, 08),
+        end: new Date(2016, 11, 08)
       }
     ]
   });
