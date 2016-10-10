e7abc6727d6bc7804c79175217f5e85c4e36a92d
Isaac Bell <isaacbell388@gmail.com>
2016-10-10 08:16:08 -0500

    STMP config

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = $to;
-$mail->Password = 'Goonernoob';
+$mail->Password = 'Goonernoob334';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 $mail->SMTPDebug = 2;
